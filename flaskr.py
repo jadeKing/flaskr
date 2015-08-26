30fd7134fb5625155d62efd55104ef14fb9aeedf
jadeKing <dev@jadeking.co.uk>
2015-08-27 00:35:51 +0100

    connected to d

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,7 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
+from flask  import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
+from contextlib import closing
 
 DATABASE = '/tmp/flaskr.db'
 DEBUG = True
@@ -9,3 +10,24 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+if __name__ == '__main__':
+    app.run()
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor()s.executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
