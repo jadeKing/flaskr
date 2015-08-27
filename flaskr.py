ec508aae2b691c3c8b34305c39c5ed4240c1d1a0
jadeKing <dev@jadeking.co.uk>
2015-08-27 01:10:08 +0100

    add login & logout

--- flaskr.py
+++ flaskr.py
@@ -47,4 +47,23 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirecturl(url_for('show_entries'))
-    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error  = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You are logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You are logged out')
+    return redirect(url_for('show_entries'))
